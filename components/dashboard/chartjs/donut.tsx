6f34d4ce6de6cc737693a2aba73da44d6868cd84
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2024-01-03 15:45:27 +0900

    feat: donut 차트 생성

---
+++ components/dashboard/chartjs/donut.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect } from "react";
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+interface DoughnutChartProps {
+  data: number[];
+  labels: string[];
+}
+
+const Donut: React.FC<DoughnutChartProps> = ({ data, labels }) => {
+  useEffect(() => {
+    const doughnutChartCanvas = document.getElementById(
+      "doughnutChart"
+    ) as HTMLCanvasElement;
+    let doughnutChartInstance: Chart<"doughnut", number[], string> | null =
+      null;
+
+    if (doughnutChartCanvas) {
+      if (Chart.getChart(doughnutChartCanvas)) {
+        Chart.getChart(doughnutChartCanvas)?.destroy();
+      }
+
+      doughnutChartInstance = new Chart<"doughnut", number[], string>(
+        doughnutChartCanvas,
+        {
+          type: "doughnut",
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                data: data,
+                backgroundColor: ["red", "yellow", "green", "blue", "purple"],
+              },
+            ],
+          },
+          options: {
+            animation: false,
+          },
+        }
+      );
+    }
+
+    return () => {
+      if (doughnutChartInstance) {
+        doughnutChartInstance.destroy();
+      }
+    };
+  }, [data, labels]);
+
+  return <canvas id="doughnutChart" width="400" height="200"></canvas>;
+};
+
+export default Donut;
