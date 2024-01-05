2e8cef5d0d96a75a2b6092117a2a4aa5935474e1
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2024-01-05 18:20:33 +0900

    fix: zppm In/Out 기능 추가

--- components/dashboard/chartjs/line/multiLine.tsx
+++ components/dashboard/chartjs/line/multiLine.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Chart, registerables } from "chart.js";
 
 Chart.register(...registerables);
@@ -11,11 +11,27 @@ interface lineChartJsProps {
 
 const MultiLine: React.FC<lineChartJsProps> = ({ data, labels }) => {
   const multiLineChartRef = useRef<HTMLCanvasElement | null>(null);
+  const [zoomCount, setZoomCount] = useState(0);
 
   useEffect(() => {
+    let multiLineChartInstance: Chart<"line", number[], string> | null = null;
     const multiLineChartCanvas = multiLineChartRef.current;
 
-    let multiLineChartInstance: Chart<"line", number[], string> | null = null;
+    const updateZoom = () => {
+      if (multiLineChartInstance) {
+        if (
+          multiLineChartInstance.options &&
+          multiLineChartInstance.options.scales
+        ) {
+          const xScale = multiLineChartInstance.options.scales.x;
+          if (xScale) {
+            xScale.min = zoomCount > 0 ? 1 : undefined;
+            xScale.max = zoomCount > 0 ? 6 : undefined;
+          }
+        }
+        multiLineChartInstance.update();
+      }
+    };
 
     if (multiLineChartCanvas) {
       if (Chart.getChart(multiLineChartCanvas)) {
@@ -42,14 +58,65 @@ const MultiLine: React.FC<lineChartJsProps> = ({ data, labels }) => {
       );
     }
 
+    updateZoom();
+
     return () => {
       if (multiLineChartInstance) {
         multiLineChartInstance.destroy();
       }
     };
-  }, [data, labels]);
+  }, [data, labels, zoomCount]);
 
-  return <canvas ref={multiLineChartRef} width="400" height="200"></canvas>;
+  const handleZoomIn = () => {
+    setZoomCount((prevZoomCount) => prevZoomCount + 1);
+    console.log("zoomIn 했습니다::::", zoomCount);
+  };
+
+  const handleZoomOut = () => {
+    setZoomCount((prevZoomCount) => Math.max(0, prevZoomCount - 1));
+    console.log("zoomOut 했습니다::::", zoomCount);
+  };
+
+  useEffect(() => {
+    console.log("zoomCount 변경:", zoomCount);
+  }, [zoomCount]);
+
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <button
+          style={{
+            backgroundColor: "#e9e9e9",
+            paddingRight: 6,
+            paddingLeft: 6,
+            borderWidth: 2,
+            borderColor: "#616161",
+            fontWeight: 700,
+            marginRight: 10,
+            borderRadius: 8,
+          }}
+          onClick={handleZoomIn}
+        >
+          +
+        </button>
+        <button
+          style={{
+            backgroundColor: "#e9e9e9",
+            paddingRight: 6,
+            paddingLeft: 6,
+            borderWidth: 2,
+            borderColor: "#616161",
+            borderRadius: 8,
+            fontWeight: 700,
+          }}
+          onClick={handleZoomOut}
+        >
+          -
+        </button>
+      </div>
+      <canvas ref={multiLineChartRef} width="400" height="200"></canvas>
+    </div>
+  );
 };
 
 export default MultiLine;
