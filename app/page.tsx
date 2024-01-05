3295d76a3d9917edcfb457e85dee5fcc8fa04960
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2024-01-05 10:34:08 +0900

    feat: gauge chart 추가

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,52 @@ import Bar from "@/components/dashboard/chartjs/bar/bar";
 import HorizontalBar from "@/components/dashboard/chartjs/bar/horizontalBar";
 import Donut from "@/components/dashboard/chartjs/donut/donut";
 import HalfDonut from "@/components/dashboard/chartjs/donut/halfDonut";
+import Gauge from "@/components/dashboard/chartjs/gauage/gauge";
+import GaugeChart1 from "@/components/dashboard/chartjs/gauage/gaugeChart1";
+import GaugeChart from "@/components/dashboard/chartjs/gauage/gaugeChart1";
+import GaugeChart2 from "@/components/dashboard/chartjs/gauage/gaugeChart2";
 import Line from "@/components/dashboard/chartjs/line/line";
 import MultiLine from "@/components/dashboard/chartjs/line/multiLine";
+import BarChart from "@/components/dashboard/d3/bar/bar";
+
+import HorizontalBarChart from "@/components/dashboard/d3/bar/horizontalBar";
+import LineChart from "@/components/dashboard/d3/line/line";
 
 export default function Home() {
   const labels = ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"];
+  const gaugeLabels = ["Level.1", "Level.2", "Level.3"];
+  const gaugeLabels2 = ["Level.1", "Level.2"];
+
   const lineChartData = [10, 20, 15, 25, 18];
   const multiLineChartData = [
     [10, 20, 15, 25, 18],
     [15, 25, 18, 30, 22],
     [8, 18, 12, 20, 15],
   ];
-  const barChartData = [15, 25, 18, 30, 22];
+  const barChartData = [15, 25, -18, 30, 22];
   const horizontalBarChartData = [15, 25, 18, -6, 22];
   const doughnutChartData = [30, 20, 10, 25, 15];
   const areaChartData = [10, -13, 3, 25, -15];
+  const gauageChartData = [200, 400, 700];
+  const gauageChartData2 = [700, 200];
+
+  const chartWidth = 400;
+  const chartHeight = 200;
+
+  const d3Data = [
+    { Country: "A", Value: 50 },
+    { Country: "B", Value: 80 },
+    { Country: "C", Value: 120 },
+    { Country: "D", Value: 10 },
+    { Country: "E", Value: 150 },
+    // { Country: "F", Value: -2000 },
+  ];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-100">
-      <h1>Chart.js</h1>
+      <div style={{ fontSize: 30, fontWeight: 700 }} className="mb-20">
+        Chart.js
+      </div>
       <div className="flex flex-row">
         <div>
           <Line data={lineChartData} labels={labels} />
@@ -41,10 +68,45 @@ export default function Home() {
           <Donut data={doughnutChartData} labels={labels} />
         </div>
       </div>
+      <div className="flex flex-row mt-12 gap-10">
+        <div>
+          <Gauge data={gauageChartData} labels={gaugeLabels} />
+        </div>
+        <div>
+          <GaugeChart1 data={gauageChartData} labels={gaugeLabels} />
+        </div>
+        <div>
+          <GaugeChart2 data={gauageChartData2} labels={gaugeLabels2} />
+        </div>
+      </div>
       <div className="flex flex-row mt-12">
         <div>
           <Bar data={barChartData} labels={labels} />
         </div>
+        <div>
+          <HorizontalBar data={horizontalBarChartData} labels={labels} />
+        </div>
+      </div>
+      <div style={{ fontSize: 30, fontWeight: 700 }} className="mb-20 mt-20">
+        D3 Chart
+      </div>
+      <div className="flex flex-row">
+        <div>
+          <LineChart
+            data={lineChartData}
+            width={chartWidth}
+            height={chartHeight}
+          />
+        </div>
+        <div>
+          <HorizontalBarChart data={d3Data} />
+        </div>
+        <div>
+          <BarChart data={d3Data} />
+        </div>
+      </div>
+      <div className="flex flex-row mt-12"></div>
+      <div className="flex flex-row mt-12">
         <div>
           <HorizontalBar data={horizontalBarChartData} labels={labels} />
         </div>
