406e7867adf41de7b0d179088353b6a772498848
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2024-01-05 18:20:56 +0900

    feat: chart 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Area from "@/components/dashboard/chartjs/area/area";
 import AreaZoom from "@/components/dashboard/chartjs/area/areaZoom";
 import Bar from "@/components/dashboard/chartjs/bar/bar";
+import BarBoth from "@/components/dashboard/chartjs/bar/barBoth";
 import HorizontalBar from "@/components/dashboard/chartjs/bar/horizontalBar";
+import StackedBar from "@/components/dashboard/chartjs/bar/stackedBar";
+import StackedBar2 from "@/components/dashboard/chartjs/bar/stackedBar2";
 import Donut from "@/components/dashboard/chartjs/donut/donut";
 import HalfDonut from "@/components/dashboard/chartjs/donut/halfDonut";
 import Gauge from "@/components/dashboard/chartjs/gauage/gauge";
@@ -11,10 +14,24 @@ import GaugeChart2 from "@/components/dashboard/chartjs/gauage/gaugeChart2";
 import Line from "@/components/dashboard/chartjs/line/line";
 import LineAddData from "@/components/dashboard/chartjs/line/lineAddData";
 import MultiLine from "@/components/dashboard/chartjs/line/multiLine";
+import Treemap from "@/components/dashboard/chartjs/treemap/treemap";
 import BarChart from "@/components/dashboard/d3/bar/bar";
 
 import HorizontalBarChart from "@/components/dashboard/d3/bar/horizontalBar";
 import LineChart from "@/components/dashboard/d3/line/line";
+
+interface TreemapDataset {
+  label: string;
+  key: string;
+  groups: string[];
+  fontColor: string;
+  fontSize: number;
+  fontWeight: string;
+  tree: any[];
+  spacing: number;
+  borderWidth: number;
+  borderColor: string;
+}
 
 export default function Home() {
   const labels = ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"];
@@ -24,9 +41,9 @@ export default function Home() {
 
   const lineChartData = [10, 20, 15, 25, 18];
   const multiLineChartData = [
-    [10, 20, 15, 25, 18],
-    [15, 25, 18, 30, 22],
-    [8, 18, 12, 20, 15],
+    [10, 20, 15, 5, 18],
+    [15, 20, 10, 24, 22],
+    [17, 33, 1, 14, 12],
   ];
   const barChartData = [15, 25, -18, 30, 22];
   const horizontalBarChartData = [15, 25, 18, -6, 22];
@@ -35,6 +52,35 @@ export default function Home() {
   const areaZoomChartData = [10, -13, 3, 25, -15, 32, -23, 3];
   const gauageChartData = [200, 400, 700];
   const gauageChartData2 = [700, 200];
+
+  const legend = ["up", "down"];
+
+  const treemapDataset: TreemapDataset = {
+    label: "treemap",
+    key: "price",
+    groups: ["displayValue"],
+    fontColor: "grey",
+    fontSize: 16,
+    fontWeight: "bold",
+    tree: [
+      { price: 100, name: "BTC" },
+      { price: 20, name: "ETX" },
+      { price: 6, name: "XRP" },
+      { price: 6, name: "DGE" },
+      { price: 0.4, name: "TRD" },
+      { price: 40, name: "TVD" },
+      { price: 10, name: "FRO" },
+      { price: 10, name: "GEO" },
+      { price: 63, name: "ABC" },
+    ].map((item) => {
+      const { name, price } = item;
+      const displayValue = `Subtheme: ${name} ${price}`;
+      return { ...item, displayValue };
+    }),
+    spacing: 0.1,
+    borderWidth: 1,
+    borderColor: "rgba(255, 255, 255, 0.5)",
+  };
 
   const chartWidth = 400;
   const chartHeight = 200;
@@ -90,6 +136,13 @@ export default function Home() {
         <div>
           <HorizontalBar data={horizontalBarChartData} labels={labels} />
         </div>
+        <div>
+          <StackedBar2
+            maleData={lineChartData}
+            femaleData={doughnutChartData}
+            labels={labels}
+          />
+        </div>
       </div>
       <div className="flex flex-row mt-12 gap-10">
         <div>
@@ -97,6 +150,9 @@ export default function Home() {
         </div>
         <div>
           <AreaZoom data={areaZoomChartData} labels={areaZoomLabels} />
+        </div>
+        <div>
+          <Treemap data={treemapDataset} />
         </div>
       </div>
       <div style={{ fontSize: 30, fontWeight: 700 }} className="mb-20 mt-20">
